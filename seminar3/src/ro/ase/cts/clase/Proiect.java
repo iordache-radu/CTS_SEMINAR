02d238eb54d101df3b82b402ebcaad4e539a1dca
iordache-radu <raducutmt@gmail.com>
2021-03-11 19:27:37 +0200

    Seminar 3

---
+++ seminar3/src/ro/ase/cts/clase/Proiect.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.clase;
+
+public class Proiect {
+
+    private int pragDeAcceptare;
+
+    public Proiect(int pragDeAcceptare) {
+        this.pragDeAcceptare = pragDeAcceptare;
+    }
+
+    public int getPragDeAcceptare() {
+        return pragDeAcceptare;
+    }
+
+    public void setPragDeAcceptare(int pragDeAcceptare) {
+        this.pragDeAcceptare = pragDeAcceptare;
+    }
+}
