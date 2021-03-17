9576d52cabb7dfff9c97567524d81843a23a9f07
iordache-radu <raducutmt@gmail.com>
2021-03-17 23:55:35 +0200

    seminar 4

---
+++ CTS_SEMINAR-4/src/ro/ase/cts/clase/readers/AngajatReader.java
@@ -0,0 +1,39 @@
+package ro.ase.cts.clase.readers;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatReader extends IReader {
+
+    public AngajatReader(String filePath) {
+        super(filePath);
+    }
+
+    public List<Aplicant> readAplicants() throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(super.filePath));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Angajat angajat = new Angajat();
+            super.citireAplicant(input2,angajat);
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+
+            angajat.setSalariu(salariu);
+            angajat.setOcupatie(ocupatie);
+
+            angajati.add(angajat);
+        }
+        input2.close();
+        return angajati;
+    }
+
+
+}
---
+++ CTS_SEMINAR-4/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,40 @@
+package ro.ase.cts.program;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.Elev;
+import ro.ase.cts.clase.Proiect;
+import ro.ase.cts.clase.readers.AngajatiReader;
+import ro.ase.cts.clase.readers.IReader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Program {
+
+	public static List<Aplicant> citesteAplicanti( IReader reader) throws FileNotFoundException {
+		return reader.readAplicants();
+	}
+
+	public static void main(String[] args) {
+        System.out.println("Suma finantare ptr angajati: " + Angajat.getSumaFinantata());
+        System.out.println("Suma finantare ptr elevi: " + Elev.getSumaFinantata());
+
+        List<Aplicant> listaAplicanti;
+		try {
+			listaAplicanti = citesteAplicanti(new AngajatiReader("angajati.txt"));
+			Proiect proiect=new Proiect(81);
+
+			for(Aplicant aplicant:listaAplicanti) {
+                System.out.println(aplicant.toString());
+                System.out.println(aplicant.getSumaFinantare());
+
+                aplicant.afisareRaspunsProiect(proiect);
+            }
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
